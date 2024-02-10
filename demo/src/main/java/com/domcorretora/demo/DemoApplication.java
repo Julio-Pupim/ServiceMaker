5867112ef031c4ad2695ef40e9059aa4fa35601b
Antônio de Oliveira Agnolin <143107544+AntonioAgnolin@users.noreply.github.com>
2024-02-10 17:57:18 -0300

    Update DemoApplication.java

--- demo/src/main/java/com/domcorretora/demo/DemoApplication.java
+++ demo/src/main/java/com/domcorretora/demo/DemoApplication.java
@@ -8,6 +8,15 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Spring Boot Version: 3.2.2 ");
+		System.out.println("Java Project: Maven ");
+		System.out.println("Java Version: 21 LTS ");
+		System.out.println("Dependencies: ");
+		System.out.println("Spring Data JPA ");
+		System.out.println("Spring Web ");
+		System.out.println("Liquidbase Migration ");
+		System.out.println("Lombok ");
+		System.out.println("Spring Security ");
 	}
 
 }
