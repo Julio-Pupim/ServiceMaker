c1cd9a93bb866f645e64f862344cf3fa393482b3
Vinicius Michels <vrpmichels@gmail.com>
2024-09-02 17:40:48 -0300

    Criado a classe Usuario
    Criado a classe usuario e mapeados a entidade conforme definido no DER.

--- src/main/java/br/com/servicemaker/Usuario.java
+++ src/main/java/br/com/servicemaker/Usuario.java
@@ -1,4 +1,36 @@
 package br.com.servicemaker;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "USUARIO")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    private String cpf;
+
+    private String senha;
+
+    private Boolean prestador;
+
+    @OneToOne(mappedBy = "usuario")
+    private Endereco endereco;
+
+    @OneToOne(mappedBy = "usuario")
+    private Contato contato;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Reserva> reservas;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Avaliacao> avaliacoes;
+
+
 }
