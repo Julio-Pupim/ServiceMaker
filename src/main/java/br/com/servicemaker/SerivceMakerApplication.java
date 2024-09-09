02e3c6d537081fd20e06bb9ad7be2bebf2b686df
julio <julio.minellamatheus@gmail.com>
2024-09-09 20:13:40 -0300

    fix: starter não pode ter construtor

--- src/main/java/br/com/servicemaker/SerivceMakerApplication.java
+++ src/main/java/br/com/servicemaker/SerivceMakerApplication.java
@@ -1,19 +1,13 @@
 package br.com.servicemaker;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class SerivceMakerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SerivceMakerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SerivceMakerApplication.class, args);
+  }
 
 }
