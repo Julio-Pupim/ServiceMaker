a07c647ba27409eee10d4590b9fed276a6f970b4
Vinicius Michels <vrpmichels@gmail.com>
2024-09-14 21:02:22 -0300

    Criacao de todas classes de controller

---
+++ src/main/java/br/com/servicemaker/controller/ServicoController.java
@@ -0,0 +1,16 @@
+package br.com.servicemaker.controller;
+
+
+import br.com.servicemaker.AbstractCrud.AbstractController;
+import br.com.servicemaker.domain.Servico;
+import br.com.servicemaker.repository.ServicoRepository;
+import br.com.servicemaker.service.ServicoService;
+
+public class ServicoController extends
+    AbstractController<Servico, ServicoRepository, ServicoService> {
+
+  public ServicoController(ServicoService service){
+    super(service);
+  }
+
+}
---
+++ src/main/java/br/com/servicemaker/controller/SetorController.java
@@ -0,0 +1,16 @@
+package br.com.servicemaker.controller;
+
+
+import br.com.servicemaker.AbstractCrud.AbstractController;
+import br.com.servicemaker.domain.Setor;
+import br.com.servicemaker.repository.SetorRepository;
+import br.com.servicemaker.service.SetorService;
+
+public class SetorController extends
+    AbstractController<Setor, SetorRepository, SetorService> {
+
+  public SetorController(SetorService service){
+    super(service);
+  }
+
+}
