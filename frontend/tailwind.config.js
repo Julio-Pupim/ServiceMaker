33f782b5ef4e19cc24d8acd09c41ea9c2ba421b8
Antônio Agnolin <antonioagnolin@proton.me>
2024-09-19 22:25:41 -0300

    Nova estrutura com expo route

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '',
+        secondary: {
+          DEFAULT: "",
+          100: "",
+          200: "",
+        },
+        black: {
+          DEFAULT: "",
+          100: "",
+          200: "",
+        },
+        gray: {
+          100: "",
+        },
+      },
+      fontFamily: {
+      },
+    },
+  },
+  plugins: [],
+}
+
