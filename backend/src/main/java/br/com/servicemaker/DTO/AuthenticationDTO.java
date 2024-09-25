98ffaa52548bd08b5d741cf35e512c9ed8a382e9
Rafael <rafael.nogueirahl@gmail.com>
2024-09-25 00:01:28 -0300

    feat: criação segurança de acesso

---
+++ backend/src/main/java/br/com/servicemaker/DTO/AuthenticationDTO.java
@@ -0,0 +1,7 @@
+package br.com.servicemaker.DTO;
+
+public record AuthenticationDTO (String login, String password){
+
+
+
+}
--- backend/src/main/java/br/com/servicemaker/controller/AutenticacaoController.java
+++ backend/src/main/java/br/com/servicemaker/controller/AutenticacaoController.java
@@ -1,6 +1,11 @@
 package br.com.servicemaker.controller;
 
+import br.com.servicemaker.DTO.AuthenticationDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,10 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/usuario")
 public class AutenticacaoController {
-    @PostMapping("/login");
-    Public ResponseEntity login(@ResponseBody @Valid Auten){
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@ResponseBody @Valid AuthenticationDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(),data.password());
+        var token = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
     }
-
 
 }
