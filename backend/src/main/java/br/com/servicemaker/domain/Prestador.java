293e193c7defe71582357ed0131d77cdd7714cc0
julio <julio.minellamatheus@gmail.com>
2024-10-14 20:51:46 -0300

    fix: import não sendo usado

--- backend/src/main/java/br/com/servicemaker/domain/Prestador.java
+++ backend/src/main/java/br/com/servicemaker/domain/Prestador.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
