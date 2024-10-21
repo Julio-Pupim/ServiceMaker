68e122c37eefae46c1d69e62fcaf2a1114ad8b8e
Julio-Pupim <julio.minellamatheus@gmail.com>
2024-10-21 20:22:22 -0300

    fix: banco de dados

--- backend/src/main/resources/db/migration/V2__correcao_banco.sql
+++ backend/src/main/resources/db/migration/V2__correcao_banco.sql
@@ -1,56 +1,15 @@
-ALTER TABLE prestador
-    DROP CONSTRAINT agenda_fk;
+alter table if exists usuario
+    add column if not exists id_contato bigint,
+    add column if not exists id_agenda  bigint;
 
-ALTER TABLE usuario
-    ADD dtype VARCHAR(31);
-
-ALTER TABLE usuario
-    ADD id_agenda BIGINT;
-
-ALTER TABLE usuario
-    ADD id_contato BIGINT;
-
-ALTER TABLE usuario
-    ADD role SMALLINT;
-
-UPDATE usuario
-SET DTYPE = 'Usuario'
-WHERE DTYPE is null;
-
-ALTER TABLE usuario
-    ALTER COLUMN id_agenda SET NOT NULL;
-
-ALTER TABLE usuario
-    ALTER COLUMN id_contato SET NOT NULL;
-
-ALTER TABLE usuario
+ALTER TABLE if exists usuario
     ADD CONSTRAINT uc_usuario_id_agenda UNIQUE (id_agenda);
 
-ALTER TABLE usuario
+ALTER TABLE if exists usuario
     ADD CONSTRAINT uc_usuario_id_contato UNIQUE (id_contato);
 
-ALTER TABLE usuario
+ALTER TABLE if exists usuario
     ADD CONSTRAINT FK_USUARIO_ON_ID_AGENDA FOREIGN KEY (id_agenda) REFERENCES agenda (id);
 
-ALTER TABLE usuario
+ALTER TABLE if exists usuario
     ADD CONSTRAINT FK_USUARIO_ON_ID_CONTATO FOREIGN KEY (id_contato) REFERENCES contato (id);
-
-DROP TABLE prestador CASCADE;
-
-ALTER TABLE usuario
-    ALTER COLUMN cpf TYPE VARCHAR(255) USING (cpf::VARCHAR(255));
-
-ALTER TABLE usuario
-    ALTER COLUMN cpf DROP NOT NULL;
-
-ALTER TABLE usuario
-    ADD CONSTRAINT uc_usuario_id_agenda UNIQUE (id);
-
-ALTER TABLE usuario
-    ALTER COLUMN nome TYPE VARCHAR(255) USING (nome::VARCHAR(255));
-
-ALTER TABLE usuario
-    ALTER COLUMN prestador DROP NOT NULL;
-
-ALTER TABLE usuario
-    ALTER COLUMN senha TYPE VARCHAR(255) USING (senha::VARCHAR(255));
