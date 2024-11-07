2316b12d4a9083c095000ed9fbd508e13f4a0cbd
PedroSorriso <pedroaugustomuniz14@gmail.com>
2024-11-07 02:00:33 -0300

    alteração da tela de prestadores

--- frontend/app/(extra)/prestador.tsx
+++ frontend/app/(extra)/prestador.tsx
@@ -2,6 +2,7 @@ import { View, Text, ScrollView, TouchableOpacity, StyleSheet, Image, StatusBar}
 import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { router } from 'expo-router';
 
 
 const Prestadores = () => {
@@ -17,33 +18,44 @@ const Prestadores = () => {
         { id: '6', nome: 'Sebastião', titulo: 'Instalação de Grama e Tapetes Verdes' },    
     ]
 
-    const prestadorClick = () =>{  
-      navigation.navigate('');
+    const servicosPrestadorClick = () =>{  
+     router.navigate('/(extra)servicosPrestador');
+    };
+    const inicioClick =()=>{
+      router.navigate('/(tabs)/inicio');
     };
     
     return(
 
     <View style = {styles.container}>
-      <StatusBar hidden/>
       <View style = {styles.topoTela}>
-        <View style={styles.userText}>
-          <Ionicons name="person-circle-outline" size={35} color="white" />
-          <Text style={styles.userName}>Prestadores</Text>
-        </View>
-      </View>
+        <TouchableOpacity onPress={inicioClick}>
+          <Ionicons name="arrow-back-outline" size={30} color="white" />
+        </TouchableOpacity>
+         
+         <View style={styles.centralTitulo}>
+            <Ionicons name="bag-handle-outline" size={35} color="white" />
+            <Text style={styles.userName}>Prestadores</Text>
+         </View>
+      </View> 
 
         <ScrollView>
            {tipoPrestador.map(prestador => (
-            <TouchableOpacity key={prestador.id} style = {styles.header} onPress={prestadorClick}>
+            <View style = {styles.header}>
               <View style = {styles.posicao}>
               <Image source={require('../../assets/images/prestador.jpg')} 
               style = {styles.imagem}/>
               <View style = {styles.textoContainer}>
                 <Text style = {styles.nomePrestador}>Nome:{prestador.nome} </Text>
                 <Text style = {styles.infoPrestador}>{prestador.titulo}</Text> 
-               </View>
-               </View>
-             </TouchableOpacity>
+              </View>
+              </View>
+              <View style={styles.botaoServicos} >
+                <TouchableOpacity key={prestador.id} onPress={servicosPrestadorClick}>
+                  <Text  style={styles.textServicos} >Mais serviços</Text>
+                </TouchableOpacity>
+              </View>
+             </View>
            ))}   
         </ScrollView>
 
@@ -64,7 +76,10 @@ const styles = StyleSheet.create({
       borderBottomLeftRadius: 20,
       borderBottomRightRadius: 20,
       padding: 25,
-      marginBottom: 2,
+      marginBottom: 20,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
     },
     userText: {
       flexDirection: 'row',
@@ -82,7 +97,7 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'space-around',
         padding: 19,
-        height: 100,
+        height: 120,
         margin: 4,
         borderRadius: 10,
     },
@@ -90,6 +105,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         width: 60,
         height: 60, 
+        borderWidth: 1,
+        borderRadius: 10
     },
     nomePrestador:{
       fontSize: 13,
@@ -106,6 +123,25 @@ const styles = StyleSheet.create({
     },
     posicao:{
       flexDirection: 'row'
+    },
+    textServicos:{
+      fontSize: 12,
+      fontWeight: 'bold',
+      
+    },
+    botaoServicos:{
+      backgroundColor: '#FFD700',
+      borderRadius: 5,
+      marginVertical: 10,
+      borderColor: 'black',
+      borderWidth: 1,
+      alignItems: 'center'
+    },
+    centralTitulo:{
+      margin:'auto',
+      flexDirection:'row',
+      alignItems:'center',
+      justifyContent:'center'
     }
 
 });
