1dace5774f4e39d7b33b417bcf1d5636dd19495d
Antônio Agnolin <antonioagnolin@proton.me>
2024-12-07 16:12:36 -0300

    tentando fazer o backend voltar a subir

--- backend/src/main/java/br/com/servicemaker/repository/ReservaRepository.java
+++ backend/src/main/java/br/com/servicemaker/repository/ReservaRepository.java
@@ -2,7 +2,6 @@ package br.com.servicemaker.repository;
 
 import br.com.servicemaker.abstractcrud.AbstractRepository;
 import br.com.servicemaker.domain.Reserva;
-import br.com.servicemaker.domain.Servico;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,6 +11,6 @@ import java.util.List;
 @Repository
 public interface ReservaRepository extends AbstractRepository<Reserva> {
 
-    @Query("SELECT r FROM Reserva r WHERE r.data = :data")
+    @Query("SELECT r FROM Reserva r WHERE r.reservadoEm = :data")
     List<Reserva> findAllReservasPorData(@Param("data") String data);
 }
