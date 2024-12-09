e459e517519b5d69a48c5905c531058febc2de80
Vinicius Michels <vrpmichels@gmail.com>
2024-12-09 19:24:00 -0300

    exclusao-linha-errada

--- backend/src/main/java/br/com/servicemaker/service/EmailService.java
+++ backend/src/main/java/br/com/servicemaker/service/EmailService.java
@@ -1,6 +1,5 @@
 package br.com.servicemaker.service;
 
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
@@ -8,7 +7,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 public class EmailService {
 
     @Autowired
