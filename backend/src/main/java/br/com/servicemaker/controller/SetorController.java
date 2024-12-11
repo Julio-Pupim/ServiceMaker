39b6954cc4110ecbe9551612dc4fcb85cfb8c52d
Julio-Pupim <julio.minellamatheus@gmail.com>
2024-12-10 21:43:43 -0300

    fix: pmd

--- backend/src/main/java/br/com/servicemaker/controller/SetorController.java
+++ backend/src/main/java/br/com/servicemaker/controller/SetorController.java
@@ -5,10 +5,7 @@ import br.com.servicemaker.abstractcrud.AbstractController;
 import br.com.servicemaker.domain.Setor;
 import br.com.servicemaker.repository.SetorRepository;
 import br.com.servicemaker.service.SetorService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SetorController extends
     AbstractController<Setor, SetorRepository, SetorService> {
 
-  public SetorController(SetorService service, SetorRepository setorRepository) {
+  public SetorController(SetorService service) {
     super(service);
   }
 
