d86cb23e0a53c26017e78a5f2e577e578740cd3f
julio pupim <julio.minellamatheus@gmail.com>
2025-10-26 17:22:00 -0300

    infra: configurando docker para projeto

---
+++ servicemaker-api/src/main/java/br/com/servicemaker/ServiceMakerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.servicemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMakerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceMakerApiApplication.class, args);
+	}
+
+}
---
+++ servicemaker-api/src/test/java/br/com/servicemaker/ServiceMakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.servicemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMakerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
